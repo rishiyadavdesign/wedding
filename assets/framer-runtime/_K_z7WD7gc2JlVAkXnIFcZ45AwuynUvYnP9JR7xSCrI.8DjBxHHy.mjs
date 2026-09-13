566c1d71b66dc9cefb1c89c6bee91a3455846a8e
Rishi Yadav <113158883+rishiyadavdesign@users.noreply.github.com>
2026-09-13 14:58:40 +0530

    Revert "Remove unused localized Framer extras"
    This reverts commit 101c8cb4afc7873e2895c4015d9bc0bc1132010a.

---
+++ assets/framer-runtime/_K_z7WD7gc2JlVAkXnIFcZ45AwuynUvYnP9JR7xSCrI.8DjBxHHy.mjs
@@ -0,0 +1,64 @@
+import{t as e}from"./rolldown-runtime.Dh6celcD.mjs";async function t(e,t,i){let a=r[e],o=a?await a(t,i):void 0,s={bodyEnd:[],bodyStart:[],headEnd:[],headStart:[]};for(let t of n){if(t.pageIds&&!t.pageIds.has(e))continue;let n=t.code(o);n&&s[t.placement].push({...t,code:n})}return s}var n,r,i,a;e((()=>{n=[{code:e=>`<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "Product",
+  "name": “Mountain – Wedding Website Invite",
+  "image": "./images/gdmN2dDmg0szS1jwXPJFJZ0xsIE-width-338-height-339.png", 
+  "video": "../mere-liye-tum-kaafi-ho.mp4",
+  "description": "A modern wedding invite that opens as a website, updates anytime, and shares instantly on WhatsApp.",
+  "brand": {
+    "@type": "Brand",
+    "name": "Missing Piece"
+  },
+  "offers": {
+    "@type": "Offer",
+    "priceCurrency": "INR",
+    "price": "3999",
+    "availability": "https://schema.org/InStock",
+    "url": "https://www.missingpieceinvites.com/product-card-mountain"
+  }
+}
+<\/script>`,id:`legacy-headEnd-I5RCBTTUN`,loadMode:`once`,name:`Custom Code (Legacy)`,pageIds:new Set([`I5RCBTTUN`]),placement:`headEnd`},{code:e=>`<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "Product",
+  "name": “Beach – Wedding Website Invite",
+  "image": "./images/gdmN2dDmg0szS1jwXPJFJZ0xsIE-width-338-height-339.png", 
+  "video": "../mere-liye-tum-kaafi-ho.mp4": "A modern wedding invite that opens as a website, updates anytime, and shares instantly on WhatsApp.",
+  "brand": {
+    "@type": "Brand",
+    "name": "Missing Piece"
+  },
+  "offers": {
+    "@type": "Offer",
+    "priceCurrency": "INR",
+    "price": "3999",
+    "availability": "https://schema.org/InStock",
+    "url": "https://www.missingpieceinvites.com/product-card-beach"
+  }
+}
+<\/script>
+
+`,id:`legacy-headEnd-hPo7KB9jd`,loadMode:`once`,name:`Custom Code (Legacy)`,pageIds:new Set([`hPo7KB9jd`]),placement:`headEnd`},{code:e=>`<script type="application/ld+json">
+{
+  "@context": "https://schema.org/",
+  "@type": "Product",
+  "name": "City – Wedding Website Invite",
+  "image": "./images/gdmN2dDmg0szS1jwXPJFJZ0xsIE-width-338-height-339.png",
+  "video": "../mere-liye-tum-kaafi-ho.mp4",
+  "description": "A modern wedding invite that opens as a website, updates anytime, and shares instantly on WhatsApp.",
+  "brand": {
+    "@type": "Brand",
+    "name": "Missing Piece"
+  },
+  "offers": {
+    "@type": "Offer",
+    "priceCurrency": "INR",
+    "price": "3999",
+    "availability": "https://schema.org/InStock",
+    "url": "https://www.missingpieceinvites.com/product-card-city"
+  }
+}
+<\/script>
+`,id:`legacy-headEnd-Vll1G35mY`,loadMode:`once`,name:`Custom Code (Legacy)`,pageIds:new Set([`Vll1G35mY`]),placement:`headEnd`}],r={},i={bodyEnd:[],bodyStart:[],headEnd:[`legacy-headEnd`,`legacy-headEnd-I5RCBTTUN`,`legacy-headEnd-hPo7KB9jd`,`legacy-headEnd-Vll1G35mY`],headStart:[`legacy-headStart-plugin-e4c361`]},a={exports:{snippetsSorting:{type:`variable`,annotations:{framerContractVersion:`1`}},getSnippets:{type:`function`,annotations:{framerContractVersion:`1`}},__FramerMetadata__:{type:`variable`}}}}))();export{a as __FramerMetadata__,t as getSnippets,i as snippetsSorting};
+//# sourceMappingURL=_K_z7WD7gc2JlVAkXnIFcZ45AwuynUvYnP9JR7xSCrI.8DjBxHHy.mjs.map
